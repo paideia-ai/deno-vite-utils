5eb55bc200d6993aca67f872eaa0f14cc281424e
Liu, Minsheng <lambda@liu.ms>
2025-05-25 16:39:00 +0800

    Clean up old files

--- example-basic/.vite/deps/react-dom.js
+++
@@ -1,5 +0,0 @@
-import {
-  require_react_dom
-} from "./chunk-OAY7NUTQ.js";
-import "./chunk-C4WSDQ3N.js";
-export default require_react_dom();
--- example-basic/.vite/deps/react.js
+++
@@ -1,4 +0,0 @@
-import {
-  require_react
-} from "./chunk-C4WSDQ3N.js";
-export default require_react();
